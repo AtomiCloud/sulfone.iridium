44016ce8703c77af8db5fb2881ce662826b9fd7d
kirinnee <kirinnee97@gmail.com>
2023-11-14 22:44:51 +0800

    feat: YUM repository

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,1 @@
-FROM rust:1.72.0
-
-ENV CROSS_DOCKER_IN_DOCKER=true
-
-RUN cargo install cross
-
-WORKDIR /app
+FROM scratch
--- scripts/fury.sh
+++ scripts/fury.sh
@@ -6,8 +6,17 @@ set -eou pipefail
 
 directory="dist"
 
+echo "⬆️ Pushing APT packages fury.io"
 # Use the find command to locate all .deb files in the directory
 find "$directory" -type f -name "*.deb" | while read -r file; do
-  echo "pushing $file to fury.io"
+  echo "⏫ pushing $file to fury.io"
   curl -F package=@"$file" "https://${FURY_TOKEN}@push.fury.io/atomicloud/"
+  echo "✅ pushed $file to fury.io"
+done
+
+echo "⬆️ Pushing YUM packages fury.io"
+find "$directory" -type f -name "*.rpm" | while read -r file; do
+  echo "⏫ pushing $file to fury.io"
+  curl -F package=@"$file" "https://${FURY_TOKEN}@push.fury.io/atomicloud/"
+  echo "✅ pushed $file to fury.io"
 done
