107c8c3c83c8670caf2315d7c7322d31892d5215
kirinnee <kirinnee97@gmail.com>
2025-05-04 14:10:06 +0800

    fix: use nix build process

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
 [ "${TARGET}" = '' ] && echo "❌ 'TARGET' env var not set" && exit 1
+[ "${BIN_NAME}" = '' ] && echo "❌ 'BIN_NAME' env var not set" && exit 1
 
 set -eou pipefail
 
 # Enable static linking
-export RUSTFLAGS="-C target-feature=+crt-static"
-cargo build --release --target "$TARGET"
+nix build .#
+
+mkdir -p "target/${TARGET}/release"
+cp "./result/bin/${BIN_NAME}" "target/${TARGET}/release/${BIN_NAME}"
