bcb1426264a3f5adff62c98ab6ec709fa34b7e68
adelphi-liong <adelphi@atomi.cloud>
2026-03-09 16:17:09 -0700

    test(e2e): update e2e fixtures to use new build format [CU-86ewvp51g]
    - Add build sections to plugin2, processor2, resolver2, template2 cyan.yaml
    - Use kirinnee (Docker Hub) as build registry
    - Update e2e.sh to use cyanprint push --build for "2" artifacts

--- e2e/e2e.sh
+++ e2e/e2e.sh
@@ -12,31 +12,35 @@ cyanprint daemon start
 # build resolvers
 echo "🔍 Publishing resolvers..."
 ./e2e/publish-resolver.sh ./e2e/resolver1 resolver1 push
-./e2e/publish-resolver.sh ./e2e/resolver1 resolver1 push
 
-./e2e/publish-resolver.sh ./e2e/resolver2 resolver2 push
-./e2e/publish-resolver.sh ./e2e/resolver2 resolver2 push
+# resolver2 uses new build format
+tag=$(openssl rand -hex 3)
+cyanprint push resolver --build "$tag" --config ./e2e/resolver2/cyan.yaml
 
 # build processors
 echo "🔍 Publishing processors..."
 ./e2e/publish-processor.sh ./e2e/processor1 processor1 push
-./e2e/publish-processor.sh ./e2e/processor1 processor1 push
 
-./e2e/publish-processor.sh ./e2e/processor2 processor2 push
-./e2e/publish-processor.sh ./e2e/processor2 processor2 push
+# processor2 uses new build format
+tag=$(openssl rand -hex 3)
+cyanprint push processor --build "$tag" --config ./e2e/processor2/cyan.yaml
 
 # build plugins
 echo "🔍 Publishing plugins..."
 ./e2e/publish-plugin.sh ./e2e/plugin1 plugin1 push
-./e2e/publish-plugin.sh ./e2e/plugin1 plugin1 push
 
-./e2e/publish-plugin.sh ./e2e/plugin2 plugin2 push
-./e2e/publish-plugin.sh ./e2e/plugin2 plugin2 push
+# plugin2 uses new build format
+tag=$(openssl rand -hex 3)
+cyanprint push plugin --build "$tag" --config ./e2e/plugin2/cyan.yaml
 
 # build templates
 echo "🔍 Publishing templates..."
 ./e2e/publish-template.sh ./e2e/template1 template1 push
-./e2e/publish-template.sh ./e2e/template2 template2 push
+
+# template2 uses new build format
+tag=$(openssl rand -hex 3)
+cyanprint push template --build "$tag" --config ./e2e/template2/cyan.yaml
+
 ./e2e/publish-template.sh ./e2e/template3 template3 push
 ./e2e/publish-template.sh ./e2e/test-batch-a-v1 test-batch-a push
 ./e2e/publish-template.sh ./e2e/test-batch-a-v2 test-batch-a push
